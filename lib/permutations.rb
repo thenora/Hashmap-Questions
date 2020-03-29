2650d734d6856b4d568861df11839234725bce02
thenora <thenora@gmail.com>
2020-03-29 15:20:33 -0700

    permutations

--- lib/permutations.rb
+++ lib/permutations.rb
@@ -1,4 +1,23 @@
 
 def permutations?(string1, string2)
-  raise NotImplementedError, "permutations? not implemented"
+  
+  return true if string1.empty? || string2.empty?
+  return false if string1.length != string2.length
+
+  array1 = string1.chars
+  array2 = string2.chars
+
+  hash = {}
+
+  array1.each do |char|
+    hash[char] = true
+  end
+
+  array2.each do |char|
+    if hash[char] == nil
+      return false
+    end
+  end
+
+  return true
 end
