83b40f89f0f8260e93b93e2f0342f0815fff26d2
thenora <thenora@gmail.com>
2020-03-29 15:40:30 -0700

    unblocked tests

--- lib/palindrome_permutation.rb
+++ lib/palindrome_permutation.rb
@@ -1,4 +1,24 @@
-
 def palindrome_permutation?(string)
-  raise NotImplementedError, "palindrome_permutation? not implemented"
+  if string.empty?
+    return true
+  end
+
+  hash = {}
+  array = string.chars
+
+  array.each do |char|
+    hash[char] += 1
+  end
+
+  count = 0
+
+  values = hash.values
+  values.each do |num|
+    if num.odd? == true
+      count += 1
+    end
+    return false if count > 1
+  end
+
+  return true
 end
